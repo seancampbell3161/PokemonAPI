f11267837344f664ea81b46d433c2556476eff95
Sean Campbell <sean.campbell3161@gmail.com>
2021-07-06 12:26:41 -0500

    modify details column, add fetch, add method to generate html

--- app.js
+++ app.js
@@ -9,4 +9,24 @@ const {url, type, id} = apiData;
 const apiUrl = `${url}${type}/${id}`;
 //const apiUrl = `${apiData.url}${apiData.type}/${apiData.id}`;
 
-console.log(apiUrl);
+fetch(apiUrl)
+    .then( (data) => data.json() )
+    .then( (pokemon) => generateHtml(pokemon) );
+
+
+const generateHtml = (data) => {
+
+    console.log(data);
+
+    const html = `
+        <div class="name">${data.name}</div>
+        <img src=${data.sprites.front_default}>
+        <div class="details">
+            <span>Height: ${data.height} ft</span>
+            <span>Weight: ${data.weight} lbs</span>
+        </div>
+    `
+
+    const pokemonDiv = document.querySelector('.pokemon');
+    pokemonDiv.innerHTML = html;
+};
